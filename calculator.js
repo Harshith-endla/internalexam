a9e2712d89ba94ecbd47976ad23d5ef20a3e022c
Harshith Varma Endla <135434865+Harshith-endla@users.noreply.github.com>
2024-12-16 14:39:19 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3005;
 
 // Use middleware to parse the request body
 app.use(express.urlencoded({ extended: true }));
